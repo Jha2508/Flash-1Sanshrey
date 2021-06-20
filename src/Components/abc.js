d46bcc116d14d747578a22bf467e0e6ddfd309c5
Jha2508 <jhakshitiz911@gmail.com>
2021-06-20 15:59:13 +0530

    initial commit with basic needed packages

---
+++ src/Components/abc.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {BiLike} from 'react-icons/bi'
+function Abc() {
+    return (
+        <div>
+          <BiLike/>  this is abc.
+        </div>
+    )
+}
+
+export default Abc
