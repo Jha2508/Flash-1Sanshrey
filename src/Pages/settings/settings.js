9cfb66a7f057719ddfabe27f644ce39346642a1b
Jha2508 <jhakshitiz911@gmail.com>
2021-07-15 23:15:02 +0530

    settings changed a bit

--- src/Pages/settings/settings.js
+++ src/Pages/settings/settings.js
@@ -1,81 +1,15 @@
 import React from 'react'
-// import './settings.css'
-import './Setting1.css'
-import DP from './img1.jpg'
-import {ImCamera} from 'react-icons/im'
+import './settings.css'
 import Sidebar from '../../Components/sidebar/sidebar'
-import logon from '../../logon.png'
-
+import cover from './cover2.png'
 function Settings() {
     return (<>
     <Sidebar/>
-<div className="settings-body marginforside">
-        <div className="Heading" >
-            <h1>Settings</h1>
-        </div>        
-        
-        <div className="Lower-part">
-        <div className="accordion accordion-flush" id="accordionFlushExample">
-
-
-  <div className="accordion-item">
-    <h2 className="accordion-header" id="flush-headingOne">
-      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-        Profile 
-      </button>
-      </h2>
-    <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-      <div className="accordion-body">
-          <div className="prfile-img">
-            <img  src={DP} alt="DP"></img>
-            <button  id="img-dp"><ImCamera/></button>
-        </div>
-          <div className="text-area">
-            <textarea placeholder="Enter NAME" className="text"/> 
-            <button className="btn-upload">Upload</button>
-          </div>
-            
-          <div className="text-area">
-            <textarea placeholder="Enter Email ID" className="text"/> 
-            <button className="btn-upload">Upload</button>
-          </div>
-      </div>
+    <div className='marginforside bg-dark SettingsPage'>
+    <img src={cover} alt='mew' className='cover'/>
     </div>
-  </div>
-
-
-
-  <div className="accordion-item">
-    <h2 className="accordion-header" id="flush-headingTwo">
-      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-        Privacy
-      </button>
-    </h2>
-    <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-      <div className="accordion-body">
-
-<form>
-          <div className="lower-form" >
-          <textarea placeholder="Current Password" className="text"  /> 
-          <textarea placeholder="New Password" className="text"/> 
-          <textarea placeholder="Re-Type new Password" className="text"/> 
-          <div className="Lower-btn">
-          <button className="btn-upload" id="save">Save Changes</button>
-          <button className="btn-upload" id="cancel">Cancel</button>
-          </div>
-          </div>   
-</form>
-
-      </div>
-    </div>
-  </div>
-
-
-</div></div>
-
-        
-</div>
-</>
+    
+    </>
 
     )
 }
